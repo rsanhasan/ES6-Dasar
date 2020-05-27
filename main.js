82d2bfa88b2178b7225ce11d211d498b4486aa1f
Nurhasan <nurhasan@protonmail.com>
2020-05-28 02:58:55 +0700

    Var and Const

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Connected")
+// Penggunaan Var
+// Var bisa redeclare dan reassignment
+var myCapitalCity = "Jakarta"
+var myCapitalCity = "Tangerang Selatan"
+console.log(myCapitalCity)
+
+// Penggunaan Const
+// Const tidak bisa redeclare dan tidak bisa reassignment
+const myLanguage = "Bahasa Indonesia"
+myLanguage = "Bahasa Inggris"
+console.log(myLanguage)
