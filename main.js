da8c92b8f911a2b4cb38899f9ee12110428474f3
Nurhasan <nurhasan@protonmail.com>
2020-05-29 01:26:26 +0700

    forEach vs Map

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 // get element from html
-const listFoods = ["Kebab", "Pizza", "Nasi Kebuli"];
-// Menampilkan data yang ada di listFood
+const listAngka = [1, 2, 3, 5, 7];
 
-// Menggunakan Loping
-for (let i = 0; i < listFoods.length; i++) {
-    console.log(listFoods[i])
-}
-
-// Menggunakan forEach
-listFoods.forEach((food) => {
-    console.log(food)
+// menggunakan forEach
+// forEach tidak return value
+const listForEach = listAngka.forEach((angka) => {
+    return angka * angka;
 })
+
+console.log(listForEach)
+
+// menggunakan map
+// Map bisa return value
+const listMap = listAngka.map((angka) => {
+    return angka * angka
+})
+
+console.log(listMap)
+
+// Final Thoughts
+// As always, the choice between map() and forEach() will depend on your use case. If you plan to change, alternate, or use the data, you should pick map(), because it returns a new array with the transformed data.
+// But, if you won't need the returned array, don't use map() - instead use forEach() or even a for loop.
